bb3b4f7a7c454dad6771d226f37f23a4875b8765
Marc Hamacek <marc.hamacek@gmail.com>
2025-12-15 18:54:45 +0100

    refactor: 💡 config files migration to TypeScript+ES6

---
+++ backend/config/database.ts
@@ -0,0 +1,12 @@
+import { Sequelize } from 'sequelize';
+import 'dotenv/config';
+
+export const sequelize = new Sequelize(
+  process.env.DB_NAME || '',
+  process.env.DB_USER || '',
+  process.env.DB_PW || '',
+  {
+    host: process.env.DB_HOST || '',
+    dialect: 'mysql',
+  },
+);
--- backend/config/multer.ts
+++ backend/config/multer.ts
@@ -1,16 +1,16 @@
 import multer from 'multer';
 
-const MIME_TYPES = {
+const MIME_TYPES: { [key: string]: string } = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png',
 };
 
 const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
+  destination: (_req, _file, callback) => {
     callback(null, 'images');
   },
-  filename: (req, file, callback) => {
+  filename: (_req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
     callback(null, name + Date.now() + '.' + extension);
